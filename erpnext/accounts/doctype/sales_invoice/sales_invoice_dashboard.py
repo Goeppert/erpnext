5f83e88cb9161ce803f9f3074bc711e1e0d09043
Vincent Nguyen <vinhbk2000@gmail.com>
2020-05-11 21:54:21 +0700

    fixed sales invoice dashboard

--- erpnext/accounts/doctype/sales_invoice/sales_invoice_dashboard.py
+++ erpnext/accounts/doctype/sales_invoice/sales_invoice_dashboard.py
@@ -3,10 +3,10 @@ from frappe import _
 
 def get_data():
 	return {
-		'fieldname': 'sales_invoice',
+		'fieldname': 'sales_invoice', 
 		'non_standard_fieldnames': {
 			'Delivery Note': 'against_sales_invoice',
-			'Journal Entry': 'reference_name',
+			'Journal Entry': 'ref_name',
 			'Payment Entry': 'reference_name',
 			'Payment Request': 'reference_name',
 			'Sales Invoice': 'return_against',
